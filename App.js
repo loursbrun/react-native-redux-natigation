fedf596a573755f04de0116b385a835fab014ada
fbrun <fabien.brun@kcitylabs.fr>
2018-12-26 16:45:23 +0100

    add redux

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { AppLoading, Asset, Font, Icon } from 'expo';
 import AppNavigator from './navigation/AppNavigator';
+import { Provider } from 'react-redux'
+import Store from './Store/configureStore'
+
 
 export default class App extends React.Component {
   state = {
@@ -19,10 +22,12 @@ export default class App extends React.Component {
       );
     } else {
       return (
-        <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          <AppNavigator />
-        </View>
+        <Provider store={Store}>
+          <View style={styles.container}>
+            {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+            <AppNavigator />
+          </View>
+        </Provider>
       );
     }
   }
